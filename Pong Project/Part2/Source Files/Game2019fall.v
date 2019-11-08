03b21ab852462ad81f516d8c8876f3aa1f4efbe1
Thebester5 <bmulholland98@gmail.com>
2019-11-07 21:27:58 -0500

    Part 2 Done

--- Pong Project/Part2/Source Files/Game2019fall.v
+++ Pong Project/Part2/Source Files/Game2019fall.v
@@ -21,43 +21,32 @@ reg [2:0] quadAr, quadBr;
 always @(posedge clk25) quadAr <= {quadAr[1:0], rota};
 always @(posedge clk25) quadBr <= {quadBr[1:0], rotb};
 
-always @(posedge clk25)begin
-	if(quadAr[2] ^ quadAr[1] ^ quadBr[2] ^ quadBr[1]) begin
-		if(quadAr[2] ^ quadBr[1]) begin
-			if(paddlePosition < 508)     // make sure the value doesn't overflow
-				paddlePosition <= paddlePosition + 3'd4;
-			else
-				paddlePosition <= paddlePosition;
-		end
-		else begin
-			if(paddlePosition > 2'd3)        // make sure the value doesn't underflow
-				paddlePosition <= paddlePosition - 3'd4;
-			else
-				paddlePosition <= paddlePosition;
-		end
+always @(posedge clk25)
+if(quadAr[2] ^ quadAr[1] ^ quadBr[2] ^ quadBr[1])
+begin
+	if(quadAr[2] ^ quadBr[1]) begin
+		if(paddlePosition < 508)        // make sure the value doesn't overflow
+			paddlePosition <= paddlePosition + 3'd4;
 	end
-	else
-		paddlePosition <= paddlePosition;
-end		
+	else begin
+		if(paddlePosition > 2'd3)        // make sure the value doesn't underflow
+			paddlePosition <= paddlePosition - 3'd4;
+	end
+end
+		
 // ball movement	
 reg [9:0] ballX;
 reg [8:0] ballY;
 reg ballXdir, ballYdir;
 reg bounceX, bounceY;
-
-
 	
 wire endOfFrame = (xpos == 0 && ypos == 480);
 	
 always @(posedge clk25) begin
-	if(Reset == 1)begin
-		ballX <= 0;
-		ballY <= 0;
-	end 
-	else if (endOfFrame) begin // update ball position at end of each frame
+	if (endOfFrame) begin // update ball position at end of each frame
 		if (ballX == 0 && ballY == 0) begin // cheesy reset handling, assumes initial value of 0
-			ballX <= 480;
-			ballY <= 300;
+			ballX <= 200;
+			ballY <= 200;
 		end
 		else begin
 			if (ballXdir ^ bounceX) 
@@ -70,11 +59,7 @@ always @(posedge clk25) begin
 			else
 				ballY <= ballY - 2'd2;	
 		end
-	end
-	else begin
-		ballY <= ballY;
-		ballX <= ballX;
-	end
+	end	
 end		
 		
 // pixel color	
@@ -117,7 +102,7 @@ always @(posedge clk25) begin
 				ballXdir <= ~ballXdir;
 			if (bounceY)
 				ballYdir <= ~ballYdir;			
-			bounceX <= 0;
+			bounceX <= 0;`
 			bounceY <= 0;
 			if (missTimer != 0)
 				missTimer <= missTimer - 1'b1;
